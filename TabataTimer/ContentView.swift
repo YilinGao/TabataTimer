bc0ac70500eec05372751b05009ed97ba3659dc1
Yilin Gao <yilin.gao@duke.edu>
2021-03-08 18:54:54 -0800

    Initial Commit

---
+++ TabataTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabataTimer
+//
+//  Created by Yilin Gao on 3/8/21.
+//  Copyright © 2021 Yilin Gao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
